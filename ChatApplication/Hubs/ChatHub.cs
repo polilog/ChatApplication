77415a59e186ea3de1c2222a1cf3210d43c67ece
Konstantin Politay <earnur97@gmail.com>
2018-10-23 20:46:02 +0300

    initial commit

---
+++ ChatApplication/Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ChatApplication.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            Clients.All.User(Context.User.Identity.Name);
+            return base.OnConnected();
+        }
+
+        public void Send(string message)
+        {
+            Clients.Caller.message("You: " + message);
+            Clients.Others.message(Context.User.Identity.Name + ":" + message);
+        }
+    }
+}
